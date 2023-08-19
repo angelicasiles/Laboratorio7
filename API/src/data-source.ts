a3d674d7cf7965365e2b3ae82d0dda1d07f2871c
angelicasiles <125415411+angelicasiles@users.noreply.github.com>
2023-08-19 14:11:40 -0600

    Update data-source.ts

--- API/src/data-source.ts
+++ API/src/data-source.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
   host: "localhost",
   port: 3306,
   username: "root",
-  password: "root",
+  password: "Utn123**",
   database: "pruebautn",
   synchronize: true,
   logging: false,
